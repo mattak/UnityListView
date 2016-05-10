586a94a76bb252f28fbdc7b1e4017619af615a7a
Takuma Maruyama <maruyama@mfac.jp>
2016-05-10 17:28:38 +0900

    create list view

---
+++ Assets/Scripts/ItemAdapter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ItemAdapter : MonoBehaviour
+{
+    public RectTransform prefab;
+
+    public void Add(string message)
+    {
+        var item = Instantiate(prefab) as RectTransform;
+        item.SetParent(this.transform, false);
+
+        var text = item.GetComponentInChildren<Text>();
+        text.text = message;
+    }
+}
